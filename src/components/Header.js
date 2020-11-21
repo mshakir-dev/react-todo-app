533eaf15f3b7486f94186bb5f6ff4d2a0a2da0e4
Muhammad Shakir <mshakir.dev@gmail.com>
2020-11-20 21:25:45 -0600

    Clean Project and Add Two Functional Components

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <Header>
+      <div>
+        <h1> React - Todo App</h1>
+      </div>
+    </Header> 
+  );
+}
+export default Header;
